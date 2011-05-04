6e72b32b630a74213e9bf1e91ceab09fbed500aa
Patrick Wagstrom <patrick@wagstrom.net>
2011-05-04 17:39:06 -0400

    added ability to process JSONP requests

--- README.md
+++ README.md
@@ -2,3 +2,63 @@
 Cross-domain security got your web development down? Welcome to simpleproxy
 where you get possible instability in exchange for a way to workaround those
 nasty security issues.
+
+## In Brief
+The simplest way to understand and think about simpleproxy is as a very
+lightweight [mod\_rewrite][mrw] wannabe. [mod\_rewrite][mrw] is an amazing piece of
+software, however it requires that you run [Apache][apache], which is heavy weight and
+often more than you need for simple web development. Unlike the [Apache][apache]/[mod\_rewrite][mrw]
+combination, simpleproxy does not serve any content, so all requests will
+be proxied to some other server.
+
+Upon receiving a request simpleproxy sequentially processes a set of routes
+looking looking for a regular expression that matches the request. When it
+finds a route that matches it rewrites the URL and performs a request to the
+server, passing through all of the headers. It's kinda dumb about how it does
+this and does do anything smart like add the X-Forwarded-For header, but hey,
+it's good enough for my development.
+
+## Sample Routes
+
+Redirect anything that starts with /couchdb/ to an underlying CouchDB server
+running on port 5984, but remove the string CouchDB:
+
+    { id: "couchdb", match: "^/couchdb/?(.*)$", rewrite: "http://localhost:5984/$1", modes: ["p"] }
+
+Redirect anything that starts with /jsonp/ to an underlying CouchDB server and
+wrap the request in a callback to the default [JSONP][jsonp] callback function of parseRequest.
+This will also modify the outgoing content-type header to application/javascript.
+
+    { id: "jsonp", match: "^/jsonp/(.*)$", rewrite: "http://localhost:5984/$1", modes: ["p","jsonp"] }
+
+Same as above, but allow the client to specify the JSONP function name by attaching
+jsonp=newFunctionName to the query. This also prevents the jsonp query parameter from
+being passed to the underlying couchdb server.
+
+    { id: "jsonp", match: "^/jsonp/(.*)$", rewrite: "http://localhost:5984/$1", modes: ["p","jsonp","remove_jsonp_query"] }
+
+Proxy PNG, JPG, and GIF requests in an images directory to a server on Amazon's S3.
+
+    { id: "images", match: "/images/([^/]+\.(png|jpg|gif))" rewrite: "http://myimages.s3.amazon.com/$1", modes: ["p"] }
+
+Pass all requests through to a local server running on port 8080. This is often useful
+as the final catch all rule. Requests that don't match a rule throw an error.
+
+    { id: "default", match: "^(.*)$", rewrite: "http://localhost:8080$1", modes: ["p"] }
+
+## Things Left to Do
+
+ * Rule chaining does not yet work. Therefore, all rules must currently have the "p" mode set.
+ * Addition of conditions similar to RewriteCond in [mod\_rewrite][mrw]
+ * Add handler functions to proxy responses. This would give the proxy the ability to dynamically
+ rewrite content on the fly.
+ * Send the X-Forwarded-For header and other appropriate headers.
+
+## Contact Information
+
+Got more questions? Find me on github as [pridkett][ghpridkett].
+
+ [mrw]: http://httpd.apache.org/docs/current/mod/mod_rewrite.html
+ [apache]: http://httpd.apache.org/
+ [jsonp]: http://en.wikipedia.org/wiki/JSONP
+ [ghpridkett]: https://github.com/pridkett
--- simpleproxy.js
+++ simpleproxy.js
@@ -1,7 +1,8 @@
 require('./config.json');
 
 var http = require('http'),
-    url = require('url');
+    url = require('url'),
+    querystring = require('querystring');
 
 var COUCHDB_PREFIX = "/couchdb";
 
@@ -15,7 +16,7 @@ for (var i=0; i < config.routes.length; i++) {
     config.routes[i]._rewriteHost = parsedRewrite.hostname;
     config.routes[i]._rewritePort = parsedRewrite.port;
     config.routes[i]._rewritePath = parsedRewrite.pathname + parsedRewrite.search;
-    console.log("Route: " + config.routes[i].id);
+    /* console.log("Route: " + config.routes[i].id);
     console.log("Regexp: " + config.routes[i]._re);
     console.log("Host: " + config.routes[i]._rewriteHost);
     console.log("Port: " + config.routes[i]._rewritePort);
@@ -23,7 +24,8 @@ for (var i=0; i < config.routes.length; i++) {
     console.log(parsedRewrite.pathname);
     console.log(parsedRewrite.search);
     console.log(parsedRewrite.hash);
-    console.log()
+    console.log();
+    */
 }
 
 /**
@@ -35,65 +37,103 @@ var createErrorPage = function(response, code, body) {
     response.end();
 }
 
+/**
+ * check to see if this route has a mode parameter
+ */
+var hasMode = function(route, mode) {
+    if (route.hasOwnProperty("modes") && route.modes instanceof Array) {
+        return (route.modes.indexOf(mode) != -1);
+    };
+    return false;
+}
+
 http.createServer(function(request, response) {
-  var proxy_request;
-  var proxy = null;
-  try {
-    var path = url.parse(request.url).pathname;
-    for (var i=0; i < config.routes.length; i++) {
-        if (path.match(config.routes[i]._re) != null) {
-            console.log("Found a match on route: " + config.routes[i].id);
-            console.log("Request: " + path);
-            console.log("New Host: " + config.routes[i]._rewritePort + ":" + config.routes[i]._rewriteHost);
-            console.log("Rewrite: " + path.replace(config.routes[i]._re, config.routes[i]._rewritePath));
-            proxy = http.createClient(config.routes[i]._rewritePort, config.routes[i]._rewriteHost);
-            proxy_request = proxy.request(request.method, path.replace(config.routes[i]._re, config.routes[i]._rewritePath), request.headers);
-            break;
+    var proxy_request;
+    var proxy = null;
+    var jsonpFunction = null;
+    try {
+        var parsedURL = url.parse(request.url, parseQueryString=true);
+        var pathRequest = parsedURL.pathname;
+        console.log();
+        console.log("base request path: " + pathRequest);
+        if (Object.keys(parsedURL.query).length > 0) {
+            pathRequest = pathRequest + "?" + querystring.stringify(parsedURL.query);
         }
-    }
-    /*
-    if (path.substr(0, COUCHDB_PREFIX.length) === COUCHDB_PREFIX) {
-          console.log("couchdb: " + path);
-          proxy = http.createClient(5984, "localhost");
-    } else {
-          console.log("tomcat: " + path);
-          proxy = http.createClient(8080, "localhost");
-          proxy_request = proxy.request(request.method, request.url, request.headers);
-    }*/
-    if (proxy == null) {
-        createErrorPage(response, 500, "<h1>No Route Found</h1>");
-        return;
-    }
+        console.log("full path request: " + pathRequest);
 
-    proxy.addListener('error', function(connectionException) {
-      if (connectionException.errno === 61) {
-        console.log("ECONNREFUSED: connection refused to " + proxy.host + ":" + proxy.port);
-        createErrorPage(response, 504, "<h1>504 Gateway Timeout</h1><p>Received a ECONNREFUSED commecting to " + proxy.host + ":" + proxy.port + ". This usually means the remote server was not responding.</p>");
-        return;
-      } else {
-        console.log("Uncaught exception: " + connectionException);
-        createErrorPage(response, 500, "<h1>500 Internal Server Error</h1><p>Received an unhandled exception:</p><pre>" + connectionException + "</pre>");
-        return;
-      }
-    });
+        for (var i=0; i < config.routes.length; i++) {
+            var route = config.routes[i];
+            if (pathRequest.match(route._re) != null) {
+                console.log("Found a match on route: " + config.routes[i].id);
+                console.log("Base Request: " + pathRequest);
+                console.log("New Host: " + route._rewriteHost + ":" + route._rewritePort);
+                if (hasMode(route, "jsonp")) {
+                    if (hasMode(route, "remove_jsonp_query") && parsedURL.query.hasOwnProperty("jsonp")) {
+                        jsonpFunction = parsedURL.query.jsonp;
+                        delete parsedURL.query.jsonp;
+                    } else {
+                        jsonpFunction = "parseRequest";
+                    }
+                }
 
-    proxy_request.addListener('response', function (proxy_response) {
-      proxy_response.addListener('data', function(chunk) {
-        response.write(chunk, 'binary');
-      });
-      proxy_response.addListener('end', function() {
-        response.end();
-      });
-      response.writeHead(proxy_response.statusCode, proxy_response.headers);
-    });
-    request.addListener('data', function(chunk) {
-      proxy_request.write(chunk, 'binary');
-    });
-    request.addListener('end', function() {
-      proxy_request.end();
-    });
-  }
-  catch (err) {
-    console.log('Uncaught Error: ' + err);
-  }
+                /* actually perform the rewriting of the URL. anything after this must NOT modify the URL */
+                var baseRequest = parsedURL.pathname;
+                if (Object.keys(parsedURL.query).length > 0) {
+                    baseRequest = baseRequest + "?" + querystring.stringify(parsedURL.query);
+                }
+                var requestPath = baseRequest.replace(route._re, route._rewritePath);
+                console.log("Rewrite Path: " + requestPath);
+
+                if (hasMode(route, "p")) {
+                    proxy = http.createClient(route._rewritePort, route._rewriteHost);
+                    proxy_request = proxy.request(request.method, requestPath, request.headers);
+                    break;
+                } else {
+                    createErrorPage(response, 500, "<h1>Unclear Directive</h1><p>I only understand proxy requests right now</p>");
+                }
+            }
+        }
+
+        if (proxy == null) {
+            createErrorPage(response, 500, "<h1>No Route Found</h1>");
+            return;
+        }
+
+        proxy.addListener('error', function(connectionException) {
+            if (connectionException.errno === 61) {
+                console.log("ECONNREFUSED: connection refused to " + proxy.host + ":" + proxy.port);
+                createErrorPage(response, 504, "<h1>504 Gateway Timeout</h1><p>Received a ECONNREFUSED commecting to " + proxy.host + ":" + proxy.port + ". This usually means the remote server was not responding.</p>");
+                return;
+            } else {
+                console.log("Uncaught exception: " + connectionException);
+                createErrorPage(response, 500, "<h1>500 Internal Server Error</h1><p>Received an unhandled exception:</p><pre>" + connectionException + "</pre>");
+                return;
+            }
+        });
+
+        proxy_request.addListener('response', function (proxy_response) {
+            proxy_response.addListener('data', function(chunk) {
+                if (jsonpFunction != null) { response.write(jsonpFunction + "(", 'binary'); }
+                response.write(chunk, 'binary');
+            });
+            proxy_response.addListener('end', function() {
+                if (jsonpFunction != null) { response.write(");", 'binary'); }
+                response.end();
+            });
+            /* this probably should not assume utf-8 for the charset, but for now it works */
+            if (jsonpFunction != null) {
+                proxy_response.headers['content-type'] = "application/javascript;charset=utf-8";
+                proxy_response.headers['content-length'] = proxy_response.headers['content-length'] + jsonpFunction.length + 3;
+            }
+            response.writeHead(proxy_response.statusCode, proxy_response.headers);
+        });
+        request.addListener('data', function(chunk) {
+            proxy_request.write(chunk, 'binary');
+        });
+        request.addListener('end', function() {
+            proxy_request.end();
+        });
+    } catch (err) {
+        console.log('Uncaught Error: ' + err);
+    }
 }).listen(config.app.port, config.app.host);
